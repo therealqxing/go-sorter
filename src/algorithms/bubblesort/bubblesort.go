f8012d4af825cffa19bb11d3656ded63026f9bb7
qixing <qxing33@gmail.com>
2019-06-14 17:42:17 +0800

    update

--- src/algorithms/bubblesort/bubblesort.go
+++ src/algorithms/bubblesort/bubblesort.go
@@ -1,27 +1,20 @@
 package bubblesort
 
 func BubbleSort(values []int) {
-	// flag := true
-
-	// for i := 0; i < len(values) - 1; i++ {
-	// 	flag = true
-	// 	for j := 0; j < len(values) - i - 1; j++ {
-	// 		if values[j] > values [j+1] {
-	// 			values[j], values[j+1] = values[j+1], values[j]
-	// 			flag = false
-	// 		}
-	// 	}
-
-	// 	if flag == true {
-	// 		break
-	// 	}
-	// }
+	flag := true
 
 	for i := 0; i < len(values) - 1; i++ {
-		for j := i+1; j < len(values); j++ {
-			if values[j] < values[i] {
-				values[i], values[j] = values[j], values[i]
+		flag = true
+		for j := 0; j < len(values) - i - 1; j++ {
+			if values[j] > values [j+1] {
+				values[j], values[j+1] = values[j+1], values[j]
+				flag = false
 			}
+		}
+
+		if flag == true {
+			//如果已经顺序对了，就不用继续冒泡排序了
+			break
 		}
 	}
 }
